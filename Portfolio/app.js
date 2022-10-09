315b84561adfd858efa943aa4099b8a3824e595e
Fred Wong <112927300+ItsYourFriendFred@users.noreply.github.com>
2022-10-08 22:14:13 -0400

    Added routes for main pages + set up bstrap & jqry

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,30 +1,32 @@
 /* 
-File Name: index.js
+File Name: app.js
 Student Name: Fred Wong
 Student ID: 301199984 
 Date: October 3, 2022
 */
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Installed third-party packages
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');  // Set when using express -e
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ Portfolio/public/Scripts/app.js
@@ -0,0 +1,20 @@
+/* 
+File Name: app.js
+Student Name: Fred Wong
+Student ID: 301199984 
+Date: October 3, 2022
+
+public/Scripts/app.js
+*/
+
+// IIFE -- Immediately Invoked Function Expression
+(function(){
+
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
