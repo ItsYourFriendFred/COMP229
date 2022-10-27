aea870c79eafe9461f5979967504c7385ffae842
Fred Wong <112927300+ItsYourFriendFred@users.noreply.github.com>
2022-10-27 18:59:51 -0400

    Separated controller logic from routes

--- Portfolio/Server/routes/index.ts
+++ Portfolio/Server/routes/index.ts
@@ -10,34 +10,25 @@ routes/index.js
 import express from 'express';
 const router = express.Router();
 
+// Import the Controller Module
+import { DisplayAboutPage, DisplayContactPage, DisplayHomePage, DisplayProjectsPage, DisplayServicesPage } from '../Controllers/index';
+
 /* GET (Display) home page. */
-router.get('/', function(req: express.Request, res: express.Response, next: express.NextFunction) {
-  res.render('index', { title: 'Home', page: 'home' });
-});
+router.get('/', DisplayHomePage);
 
 /* GET home page, in case /home is attempted */
-router.get('/home', function(req: express.Request, res: express.Response, next: express.NextFunction) {
-  res.render('index', { title: 'Home', page: 'home' });
-});
+router.get('/home', DisplayHomePage);
 
 /* GET About Me page. */
-router.get('/about', function(req: express.Request, res: express.Response, next: express.NextFunction) {
-  res.render('index', { title: 'About', page: 'about' });
-});
+router.get('/about', DisplayAboutPage);
 
 /* GET Projects page. */
-router.get('/projects', function(req: express.Request, res: express.Response, next: express.NextFunction) {
-  res.render('index', { title: 'Projects', page: 'projects' });
-});
+router.get('/projects', DisplayProjectsPage);
 
 /* GET Services page. */
-router.get('/services', function(req: express.Request, res: express.Response, next: express.NextFunction) {
-  res.render('index', { title: 'Services', page: 'services' });
-});
+router.get('/services', DisplayServicesPage);
 
 /* GET Contact page. */
-router.get('/contact', function(req: express.Request, res: express.Response, next: express.NextFunction) {
-  res.render('index', { title: 'Contact', page: 'contact' });
-});
+router.get('/contact', DisplayContactPage);
 
 export default router;
