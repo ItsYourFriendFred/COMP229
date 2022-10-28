fe1a565526bb86a90210d23a54760c25262d2085
Fred Wong <112927300+ItsYourFriendFred@users.noreply.github.com>
2022-10-27 22:15:09 -0400

    Added Contact model

---
+++ Portfolio/Server/Models/contact.ts
@@ -0,0 +1,21 @@
+// Import Mongoose
+import mongoose from "mongoose";
+const Schema = mongoose.Schema; // Alias for mongoose.Schema
+
+// Create a Schema that matches the data
+const ContactSchema = new Schema(
+    {
+        ContactName: String,
+        ContactNumber: String,
+        EmailAddress: String
+    },
+    {
+        collection: "contact"
+    }
+);
+
+// Create a Model using the Schema
+const Model = mongoose.model("Business Contacts", ContactSchema);
+
+// Export the Model (to create a module from this file)
+export default Model;
