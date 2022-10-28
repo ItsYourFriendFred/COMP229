f097f17d5badf935c9f31bd1837afd96c9cc385b
Fred Wong <112927300+ItsYourFriendFred@users.noreply.github.com>
2022-10-27 22:19:28 -0400

    added db config

---
+++ Portfolio/Server/Config/db.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LocalURI = void 0;
+exports.LocalURI = "mongodb://localhost:27017/contacts";
+//# sourceMappingURL=db.js.map
