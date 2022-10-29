7f3b23bad71f06aa77233196e7dd36c8db4783cf
Fred Wong <112927300+ItsYourFriendFred@users.noreply.github.com>
2022-10-28 21:46:37 -0400

    Added Utility functions

---
+++ Portfolio/Server/Util/index.ts
@@ -0,0 +1,27 @@
+/* 
+File Name: index.ts
+Student Name: Fred Wong
+Student ID: 301199984 
+Date: October 3, 2022
+
+Util/index.ts
+*/
+
+import express from 'express';
+
+// Convenience function to return the DisplayName of the User
+export function UserDisplayName(req: express.Request): string {
+    if(req.user) {
+        let user = req.user as UserDocument
+        return user.DisplayName.toString();
+    }
+    return '';
+}
+
+// Helper function for guarding secure locations
+export function AuthGuard(req: express.Request, res: express.Response, next: express.NextFunction) {
+    if(!req.isAuthenticated) {
+        return res.redirect('/login')
+    }
+    next();
+}
