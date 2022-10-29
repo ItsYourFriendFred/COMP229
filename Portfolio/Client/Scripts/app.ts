a032e0cdfd9636aa444e79fa67d35025204888fa
Fred Wong <112927300+ItsYourFriendFred@users.noreply.github.com>
2022-10-29 06:30:02 -0400

    Added confirmation when deleting business contact

--- Portfolio/Client/Scripts/app.ts
+++ Portfolio/Client/Scripts/app.ts
@@ -14,24 +14,32 @@ public/Scripts/app.js
     {
         console.log("App Started...");
 
-        // use AJAX to read JSON file
+        // // use AJAX to read JSON file
 
-        // Step 1: Create a new XHR object
-        let XHR = new XMLHttpRequest();
+        // // Step 1: Create a new XHR object
+        // let XHR = new XMLHttpRequest();
 
-        // Step 2: Create the request
-        XHR.open("GET", "../Data/data.json");
+        // // Step 2: Create the request
+        // XHR.open("GET", "../Data/data.json");
 
-        // Step 3: Send the request
-        XHR.send();
+        // // Step 3: Send the request
+        // XHR.send();
 
-        // Step 4: create an event listener/handler
-        XHR.addEventListener("readystatechange", function()
-        {
-            if(XHR.readyState == 4 && XHR.status == 200)
+        // // Step 4: create an event listener/handler
+        // XHR.addEventListener("readystatechange", function()
+        // {
+        //     if(XHR.readyState == 4 && XHR.status == 200)
+        //     {
+        //         console.log("JSON Data:");
+        //         console.log(XHR.responseText);
+        //     }
+        // });
+
+        $("a.delete").on("click", function(event){
+            if(!confirm("Are you sure?"))
             {
-                console.log("JSON Data:");
-                console.log(XHR.responseText);
+                event.preventDefault();
+                location.href = "/business-contacts"
             }
         });
     }
